af93d2795e48c6dfb20e40ed83d3893eb3e748a3
niceAndGoodOnline <felix.ruttan@gmail.com>
2023-08-23 07:04:49 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let tree1 = document.getElementById("tree1");
 let tree2 = document.getElementById("tree2");
 let tree3 = document.getElementById("tree3");
+let recipe = document.getElementById("recipe");
 let bowImage = document.getElementById("bowImage");
 tree1.style.display = "none";
 tree2.style.display = "none";
 tree3.style.display = "none";
+recipe.style.display = "none";
 bowImage.style.display = "none";
 
 function toggleTree1Image() {
@@ -28,6 +30,14 @@ function toggleTree3Image() {
     	tree3.style.display = "block";
   	} else {
     	tree3.style.display = "none";
+  	}
+}
+
+function toggleRecipeImage() {
+	if (recipe.style.display === "none") {
+    	recipe.style.display = "block";
+  	} else {
+    	recipe.style.display = "none";
   	}
 }
 
